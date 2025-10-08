c3d8830bc9aa0f3c7b85a9b3c9fb4d2fc834b773
armanetl <crocfrisk38@hotmail.fr>
2025-10-08 09:16:10 +0200

    Preparation seance enfants Octobre

---
+++ Projets_Enfants/Mini_Serre/Seance_2_Octobre_Previsionnelle/main_exemple_dht11.py
@@ -0,0 +1,19 @@
+from machine import Pin
+from time import sleep
+import dht
+
+
+capteur = dht.DHT11(Pin(22)) 
+
+while True:
+#    try:
+        sleep(1)
+        # Le DHT11 renvoie au maximum une mesure toute les 1s
+        capteur.measure()
+        # Récupère les mesures du capteur
+        print(f"Temperature : {capteur.temperature():.1f}")
+        print(f"Humidite    : {capteur.humidity():.1f}")
+        # Transmet la température sur la console de l'ordinateur
+#    except OSError as e:
+#        print('Echec reception')
+        # Si la pico ne reçoit pas les mesures du capteur
