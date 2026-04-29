a7847afe54a0c9f5f71b9b3580fb8e48dfa82551
armanetl <crocfrisk38@hotmail.fr>
2026-04-29 19:12:59 +0200

    Mise a jour avec la seance finale d'Avril

--- Projets_Enfants/Mini_Serre/Seance_8_Avril/260427 code_relais_brumisateur_2bps.py
+++ Projets_Enfants/Mini_Serre/Seance_8_Avril/260427 code_relais_brumisateur_2bps.py
@@ -14,23 +14,32 @@ relais2_brumisateur = Pin(7, Pin.OUT)
 
 
 # place les actionneur dans leur etat par defaut
-relais2_brumisateur.value(1)  # eteint la pompe
-led_etat.value(0)       # eteint la led
+relais2_brumisateur.value(1)  # eteint le brumisateur
+led_etat.value(1)       # eteint la led
 mon_etat = "eteint"     # variable d'etat du programme
 
 # boucle principale
 while True:
     if mon_etat == "eteint":
         if bp3_allumer.get_state() == "appuyer":
-            # on allume la pompe
+            # on allume le brumisateur
             relais2_brumisateur.value(0)
-            led_etat.value(1)
+            time.sleep(0.05)
+            relais2_brumisateur.value(1)
+            led_etat.value(0)
             mon_etat = "allumer"
     else:
         if bp4_eteindre.get_state() == "appuyer":
-            # on eteint la pompe
+            # on eteint le brumisateur
+            relais2_brumisateur.value(0)
+            time.sleep(0.1)
             relais2_brumisateur.value(1)
-            led_etat.value(0)
+            time.sleep(0.1)
+            relais2_brumisateur.value(0)
+            time.sleep(0.1)
+            relais2_brumisateur.value(1)
+            
+            led_etat.value(1)
             mon_etat = "eteint"
 
 
--- Projets_Enfants/Mini_Serre/Seance_8_Avril/260427 code_relais_pompe_2bps.py
+++ Projets_Enfants/Mini_Serre/Seance_8_Avril/260427 code_relais_pompe_2bps.py
@@ -15,7 +15,7 @@ relais1_pompe = Pin(6, Pin.OUT)
 
 # place les actionneur dans leur etat par defaut
 relais1_pompe.value(1)  # eteint la pompe
-led_etat.value(0)       # eteint la led
+led_etat.value(1)       # eteint la led
 mon_etat = "eteint"     # variable d'etat du programme
 
 # boucle principale
@@ -24,12 +24,12 @@ while True:
         if bp1_allumer.get_state() == "appuyer":
             # on allume la pompe
             relais1_pompe.value(0)
-            led_etat.value(1)
+            led_etat.value(0)
             mon_etat = "allumer"
     else:
         if bp2_eteindre.get_state() == "appuyer":
             # on eteint la pompe
             relais1_pompe.value(1)
-            led_etat.value(0)
+            led_etat.value(1)
             mon_etat = "eteint"
 
