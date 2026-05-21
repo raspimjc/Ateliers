77e6af1e8cbcec2c4af681ad783b2e060c7d155a
armanetl <crocfrisk38@hotmail.fr>
2026-05-21 22:53:05 +0200

    Nettoyage de code

--- Projets_Enfants/Mini_Serre/Seance_9_Mai_dev/code_boucle_regulation_humidite.py
+++ Projets_Enfants/Mini_Serre/Seance_9_Mai_dev/code_boucle_regulation_humidite.py
@@ -14,19 +14,26 @@ def afficher_humidite( valeur ):
     print(f"humidite : {valeur:.1f}%")
     # A FAIRE utiliser le bandeau de led
 
-def allumer_brumisateur( brum ):
-    brum.value(0)
+def allumer_brumisateur( relais ):
+    relais.value(0)
     time.sleep(0.1)
-    brum.value(1)
+    relais.value(1)
 
-def eteindre_brumisateur( brum ):
-    brum.value(0)
+def eteindre_brumisateur( relais ):
+    relais.value(0)
     time.sleep(0.1)
-    brum.value(1)
+    relais.value(1)
     time.sleep(0.5)
-    brum.value(0)
+    relais.value(0)
     time.sleep(0.1)
-    brum.value(1)
+    relais.value(1)
+    
+def allumer_ventilateur( relais ):
+    relais.value(0)
+
+def eteindre_ventilateur( relais ):
+    relais.value(1)
+
 
 # PROGRAMME PRINCIPAL
 
@@ -40,17 +47,20 @@ relais2_brumisateur = Pin(7, Pin.OUT)
 DHT = dht.DHT22(Pin(22)) 
 time.sleep(1) # attente que le capteur humidité soit opérationnel
 
-# place les actionneur dans leur etat par defaut
-relais4_ventilateur.value(1)    # eteint le ventilateur
+# place les actionneurs dans leur etat par defaut
+eteindre_ventilateur(relais4_ventilateur)  # eteint le ventilateur
+
 mon_etat = "attente"     # variable d'etat du programme : "humidifie" / "attente" / "ventille"
 
 # boucle principale
 while True:
     # on prend la mesure
     DHT.measure()
+    
     # on affiche le niveau d'humidite
     afficher_humidite( DHT.humidity() )
-    print(f"etat {mon_etat}")
+    #print(f"etat {mon_etat}")
+    
     # en fonction de notre etat
     if mon_etat == "attente":
         # si l'humidite est en dessous du seuil bas
@@ -61,9 +71,10 @@ while True:
             mon_etat = "humidifie"
         elif DHT.humidity() > SEUIL_HUMIDITE_HAUT:
             # on allume le ventilateur
-            relais4_ventilateur.value(0)
+            allumer_ventilateur(relais4_ventilateur)
             # on indique notre nouvel etat
             mon_etat = "ventille"
+            
     elif mon_etat == "humidifie":
         # on regarde si on est passe au dessus du seuil d'humidite pour arrêter le brumisateur
         if DHT.humidity() > SEUIL_HUMIDITE_BAS:
@@ -71,11 +82,12 @@ while True:
             eteindre_brumisateur(relais2_brumisateur)
             # on indique notre nouvel etat
             mon_etat = "attente"
+            
     elif mon_etat == "ventille":
         # on regarde si on est passe en dessous du seuil d'humidite pour arrêter le ventilateur
         if DHT.humidity() < SEUIL_HUMIDITE_HAUT:
             # on eteint le ventilateur
-            relais4_ventilateur.value(1)
+            eteindre_ventilateur(relais4_ventilateur)
             # on indique notre nouvel etat
             mon_etat = "attente"
 
